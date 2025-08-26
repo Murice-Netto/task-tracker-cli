44a5f1be9bbe1dcb53bfda0f252fc4fae5c80650
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-08-26 11:44:17 -0300

    feat: adds markDone command

---
+++ src/commands/markDone.ts
@@ -0,0 +1,13 @@
+import { Command } from "../entities/Command.ts";
+import { TaskService } from "../service/Task.service.ts";
+
+export const markDone: Command = {
+  name: "mark-done",
+  description: "",
+  exec: (args: string[], service: TaskService) => {
+    if (args.length < 1) throw new Error("Missing task id.");
+    const id = Number(args[0]);
+    service.udpateTaskStatus(id, "DONE");
+    console.log("Task status updated!");
+  },
+};
