f62beb4341fb36654e46b48bbdedf7c25a835843
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-08-26 11:42:11 -0300

    feat: adds deleteTask command

---
+++ src/commands/deleteTask.ts
@@ -0,0 +1,13 @@
+import { Command } from "../entities/Command.ts";
+import { TaskService } from "../service/Task.service.ts";
+
+export const deleteTask: Command = {
+  name: "delete",
+  description: "",
+  exec: (args: string[], service: TaskService) => {
+    if (args.length < 1) throw new Error("Missing task id.");
+    const id = Number(args[0]);
+    service.deleteTask(id);
+    console.log("Task deleted!");
+  },
+};
