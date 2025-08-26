5ba067ba842fd5d5fe26e17aa0a2914640a2c765
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-08-26 11:43:27 -0300

    feat: adds markInProgress command

---
+++ src/commands/markInProgress.ts
@@ -0,0 +1,13 @@
+import { Command } from "../entities/Command.ts";
+import { TaskService } from "../service/Task.service.ts";
+
+export const markInProgress: Command = {
+  name: "mark-in-progress",
+  description: "",
+  exec: (args: string[], service: TaskService) => {
+    if (args.length < 1) throw new Error("Missing task id.");
+    const id = Number(args[0]);
+    service.udpateTaskStatus(id, "IN-PROGRESS");
+    console.log("Task status updated!");
+  },
+};
