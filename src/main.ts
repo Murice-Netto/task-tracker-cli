a2269acc6bb9f9b8e5be1eb8c12224b35a767824
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-08-26 11:48:04 -0300

    feat: adds command to the Cli

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,9 @@
 import { addTask } from "./commands/addTask.ts";
+import { deleteTask } from "./commands/deleteTask.ts";
+import { listTasks } from "./commands/listTasks.ts";
+import { markDone } from "./commands/markDone.ts";
+import { markInProgress } from "./commands/markInProgress.ts";
+import { updateTask } from "./commands/updateTask.ts";
 import { TaskService } from "./service/Task.service.ts";
 
 import { Cli } from "./utils/Cli.ts";
@@ -8,7 +13,8 @@ function main() {
   const jsonDatabase: JsonDatabase = new JsonDatabase();
   const taskService: TaskService = new TaskService(jsonDatabase);
   const app: Cli = new Cli(taskService);
-  app.addCommand(addTask);
+  app.addCommand(addTask).addCommand(deleteTask).addCommand(listTasks)
+    .addCommand(markDone).addCommand(markInProgress).addCommand(updateTask);
   app.run(Deno.args);
 }
 
