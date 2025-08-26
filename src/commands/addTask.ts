dce252956bd113a5c72d6cee3c628daa000ef636
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-08-26 12:54:26 -0300

    feat: adds CommandHandler type + applies to addTask command

--- src/commands/addTask.ts
+++ src/commands/addTask.ts
@@ -1,13 +1,18 @@
-import { Command } from "../entities/Command.ts";
-import { TaskService } from "../service/Task.service.ts";
+import { Command, CommandHandler } from "../entities/Command.ts";
+import { TaskService } from "../service/TaskService.ts";
+
+const handler: CommandHandler = async (
+  args: string[],
+  service: TaskService,
+): Promise<void> => {
+  if (args.length < 1) throw new Error("Missing task description.");
+  const description = args[0];
+  await service.create(description);
+  console.log("Task created!");
+};
 
 export const addTask: Command = {
   name: "add",
   description: "",
-  exec: (args: string[], service: TaskService) => {
-    if (args.length < 1) throw new Error("Missing task description.");
-    const description = args[0];
-    service.create(description);
-    console.log("Task created!");
-  },
+  exec: handler,
 };
