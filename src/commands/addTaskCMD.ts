af1548c0019bcc83952dce5f24dc1bd151f9e774
Antonio Mauricio <mauricio.almeida.dev@gmail.com>
2025-08-26 13:08:02 -0300

    refactor: addTaskCMD

--- src/commands/addTaskCMD.ts
+++ src/commands/addTaskCMD.ts
@@ -1,3 +1,6 @@
+import { MissingTaskPropertyError } from "../errors/MissingTaskPropertyError.ts";
+import { InvalidDataError } from "../errors/InvalidDataError.ts";
+
 import { Command, CommandHandler } from "../entities/Command.ts";
 import { TaskService } from "../service/TaskService.ts";
 
@@ -5,14 +8,20 @@ const handler: CommandHandler = async (
   args: string[],
   service: TaskService,
 ): Promise<void> => {
-  if (args.length < 1) throw new Error("Missing task description.");
+  if (args.length < 1) {
+    throw new MissingTaskPropertyError("Missing task description.");
+  }
   const description = args[0];
+  if (description === "") {
+    throw new InvalidDataError("Task description cannot be empty.");
+  }
   await service.create(description);
-  console.log("Task created!");
+  console.log("✅ Task created!");
 };
 
 export const addTaskCMD: Command = {
   name: "add",
-  description: "",
+  description: "adds a new task to the database",
+  example: "task-tracker add '[TASK-DESCRIPTION]'",
   exec: handler,
 };
